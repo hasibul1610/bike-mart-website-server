103afb0d1ecd4396696b9c290ff5ec7f4fdd8ca4
hasibul1610 <bcse.hasibul@gmail.com>
2021-11-18 15:47:57 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+
+const port = process.env.PORT || 5000;
+
+
+app.get('/', (req, res) => {
+    res.send("Bike Mart Backend Running");
+})
+
+app.listen(port, () => {
+    console.log('Server Running on PORT', port);
+})
